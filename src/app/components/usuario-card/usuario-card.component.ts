cf24773d191efd1ceea8ad543ab31266517a2825
Jorge T <jor.tec.vill@gmail.com>
2026-07-14 10:40:42 -0600

    Actualizacion: Ver mas en tarjetas

--- src/app/components/usuario-card/usuario-card.component.ts
+++ src/app/components/usuario-card/usuario-card.component.ts
@@ -1,10 +1,11 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 import { Usuario } from '../../models/usuario';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-usuario-card',
@@ -13,7 +14,8 @@ import { MatDividerModule } from '@angular/material/divider';
     MatButtonModule,
     MatCardModule,
     MatIconModule,
-    MatDividerModule
+    MatDividerModule,
+    MatExpansionModule
   ],
   templateUrl: './usuario-card.component.html',
   styleUrl: './usuario-card.component.css'
@@ -22,6 +24,8 @@ export class UsuarioCardComponent {
   usuario = input.required<Usuario>();
   editar = output<Usuario>();
   eliminar = output<number>();
+
+  readonly panelOpenState = signal(false);
 
   eliminarUsuario(): void {
     this.eliminar.emit(this.usuario().id);
